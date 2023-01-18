6abdf671b1322b4399748ba6f39b232202f52130
Santiago Velez <svelezsaffon@gmail.com>
2023-01-18 13:56:26 -0500

    Compare here

--- sh/helpers/linux.sh
+++ sh/helpers/linux.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 'Configuring additional steps'
 
 PLATFORM=$1
@@ -5,5 +7,23 @@ PLATFORM=$1
 SCONS_PARAMS=$2
 
 echo "this fedora ${FINAL_IMAGE}"
+
+
+if [ "$PLATFORM" = "linux" ]; then
+    
+    echo "Deafaulting to x86_32 CPU architecture"
+    
+
+elif [ "$PLATFORM" = "windows" ]; then
+    
+    echo "Deafaulting to X86_64 CPU architecture"
+    
+    
+else
+    echo "Unknown architecture ${ARCHITECTURE}"
+    exit 1
+fi
+
+
 
 scons -j6 platform=${PLATFORM} ${SCONS_PARAMS}
