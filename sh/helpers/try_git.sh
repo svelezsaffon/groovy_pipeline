4abc099bd83e47e91c47b3323bdb70e44fcc96aa
Santiago Velez <svelezsaffon@gmail.com>
2023-03-03 09:07:04 -0500

    try this

--- sh/helpers/try_git.sh
+++ sh/helpers/try_git.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-eval `ssh-agent -s`
+eval $(ssh-agent -s)
 
-ssh-add - <<< "$GIT_TOKEN"
+echo "$GIT_TOKEN" >> id_rsa
+
+chmod 400 id_rsa
+
+cat id_rsa
+
+ssh-add id_rsa
 
 git clone git@github.com:svelezsaffon/godot_docker.git
