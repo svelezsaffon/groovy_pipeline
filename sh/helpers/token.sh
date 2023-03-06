f0be4387bfcbf4cbcd4113033fadaaf965f4a3f6
Santiago Velez <svelezsaffon@gmail.com>
2023-03-06 10:07:35 -0500

    Add host as variable

--- sh/helpers/token.sh
+++ sh/helpers/token.sh
@@ -25,11 +25,37 @@ HELP_USAGE
 
 parse_and_clone(){
 
+# Extract the protocol (includes trailing "://").
+PARSED_PROTO="$(echo $REPOSITORY | sed -nr 's,^(.*://).*,\1,p')"
+
+# Remove the protocol from the URL.
+PARSED_URL="$(echo ${REPOSITORY/$PARSED_PROTO/})"
+
+# Extract the user (includes trailing "@").
+PARSED_USER="$(echo $PARSED_URL | sed -nr 's,^(.*@).*,\1,p')"
+
+# Remove the user from the URL.
+PARSED_URL="$(echo ${PARSED_URL/$PARSED_USER/})"
+
+# Extract the port (includes leading ":").
+PARSED_PORT="$(echo $PARSED_URL | sed -nr 's,.*(:[0-9]+).*,\1,p')"
+
+# Remove the port from the URL.
+PARSED_URL="$(echo ${PARSED_URL/$PARSED_PORT/})"
+
+# Extract the path (includes leading "/" or ":").
+PARSED_PATH="$(echo $PARSED_URL | sed -nr 's,[^/:]*([/:].*),\1,p')"
+
+# Remove the path from the URL.
+PARSED_HOST="$(echo ${PARSED_URL/$PARSED_PATH/})"
+
+
+
 eval $(ssh-agent)
 
 mkdir ~/.ssh
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan "$PARSED_HOST" >> ~/.ssh/known_hosts
 
 ssh-add - <<< "$GIT_TOKEN"
 
